9db628e5dc75e16e17cb4de87275ef5826a62479
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-06-29 11:35:25 +0100

    static fix 32

--- busy/settings.py
+++ busy/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'busy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(PROJECT_ROOT, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
