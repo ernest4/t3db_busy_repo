0055187687732db639120fd8be9916f30b9689ea
Daragh O'Farrell <daragh.ofarrell1@ucdconnect.ie>
2018-07-30 14:16:10 +0100

    Timetable function completed for planner. Needs testing

--- busyapp/ml.py
+++ busyapp/ml.py
@@ -83,14 +83,39 @@ def getLiveBusInfo(stop_id, route_id):
             return null
 """
 # Function to get timetable information in the future
-def getTimetableInfo(stop_id, route_id, datetime):
+def getTimetableInfo(stop_id, route_id, day_time):
 
-    # NOTE date time for URL must be in the format 'YYYY-MM-DDTHH:mm:ss'
-
-    r = requests.get("https://data.dublinked.ie/cgi-bin/rtpi/timetableinformation?operator=bac&type=week&stopid=768&routeid=46a&format=json")
+    # # NOTE date time for URL must be in the format 'YYYY-MM-DDTHH:mm:ss' ISO format.
+    # datetime = datetime.isoformat()
+    #
+    r = requests.get("https://data.dublinked.ie/cgi-bin/rtpi/timetableinformation?operator=bac&type=week&"
+                     "stopid="+stop_id+"&routeid="+route_id+"&format=json")
     if r.status_code == requests.codes.ok:
-        data = json.loads(r.content.decode('utf-8'))
+        data = json.loads(r.content)
+        day = day_time.weekday()
+        # Find timetable for Monday to Friday
+        if day>=0 and day<=4:
+            timetable = data['results'][1]['departures']
+        # Saturday
+        elif day = 5:
+            timetable = data['results'][1]['departures']
+        # Sunday
+        elif day = 6:
+            timetable = data['results'][1]['departures']
 
+        # Convert input time to seconds
+        input_time = day_time.hour*3600 + day_time.minute*60
+
+        # Convert timetable times to seconds
+        timetable_seconds = [(int(x.split(':')[0])*3600 + int(x.split(':')[1])*60) for x in timetable]
+
+        # Find index of closest time
+        i_time = min(range(len(timetable_seconds)), key=lambda i: abs(timetable_seconds[i] - input_time))
+
+        return timetable[i_time]
+
+    else:
+        return null
 
 # Function to get the events of a certain day
 def getEvents(date):
