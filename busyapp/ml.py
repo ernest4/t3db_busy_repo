3846554d95a7d4c12344f2c1dfbe7337e06f3797
Ernestas Monkevicius <ernestas.monkevicius@ucdconnect.ie>
2018-07-31 16:02:18 +0100

    future weather retrieval and caching complete

--- busyapp/ml.py
+++ busyapp/ml.py
@@ -8,56 +8,105 @@ import time
 import psycopg2
 from pprint import pprint
 from sklearn.externals import joblib
-
 from busy.settings import STATIC_ROOT
-
-#testing pickle
-
-#test_list = ['one', '2', 'three']
-
-#writing pickle
-#with open("./ml_models/test_file",'wb') as outputFile:
-#    pickle.dump(test_list, outputFile)
-
-#loading pickle
-#with open("./ml_models/test_file", 'rb') as inputFile:
-#    loaded_list = pickle.load(inputFile)
-
-#print(loaded_list)
+import requests
+import datetime
+import os
 
 #get weather information
 hourSinceLastCall = 0 # type: float
 weatherCode = 0 # type: int
-def getWeather(weekdayNumber: int = None):
+futureWeatherCodes = {}
+def getWeather(timeStamp: int = None):
     global hourSinceLastCall
     global weatherCode
+    global futureWeatherCodes
 
     #Dublin id for open weather: 7778677
-    def fetchRealTimeWeatherCode():
-        r = requests.get('http://api.openweathermap.org/data/2.5/weather',
-                         params={'q': 'dublin', 'APPID': os.environ.get('APPID')})
-        if r.status_code == requests.codes.ok:
-            weatherData = r.json()
-            weatherCode = weatherData['weather'][0]['id']
-            return weatherCode
-        else:
-            #Could not get weather
-            #Use 'typical' Irish weather, i.e. '801, few clouds'
-            return 801
+    def fetchRealTimeWeatherCode(timeStamp: int = None):
+        global futureWeatherCodes
+
+        if timeStamp is None: #Default to providing current weather
+            r = requests.get('http://api.openweathermap.org/data/2.5/weather',
+                             params={'q': 'dublin', 'APPID': os.environ.get('APPID')})
+
+            if r.status_code == requests.codes.ok:
+                weatherData = r.json()
+                weatherCode = weatherData['weather'][0]['id']
+                return weatherCode
+            else:
+                #Could not get weather
+                #Use 'typical' Irish weather, i.e. '801, few clouds'
+                return 801
+        else: #Make a forecast based on provided timestamp
+            r = requests.get('http://api.openweathermap.org/data/2.5/forecast',
+                             params={'id': '7778677', 'APPID': os.environ.get('APPID')})
+
+            if r.status_code == requests.codes.ok:
+                weatherData = r.json()
+
+                futureWeatherCodes = {} #Empty the dictionary
+                for row in weatherData['list']:
+                    #print(row['dt'],':',row['weather'][0]['id']) #FOR TESTING
+                    #Update the cache
+                    futureWeatherCodes[row['dt']] = row['weather'][0]['id']
+
+                weatherCode = getCachedFutureWeather(timeStamp)
+                return weatherCode
+            else:
+                # Could not get weather
+                # Use 'typical' Irish weather, i.e. '801, few clouds'
+                return 801
+
+    def getCachedFutureWeather(timeStamp: int = None):
+        index = 0
+        for key_dt in futureWeatherCodes:
+            print(key_dt, futureWeatherCodes[key_dt])
+            if index > 0:
+                next_dt = key_dt
+                if timeStamp < next_dt:
+                    futureWeatherCode = futureWeatherCodes[last_dt] #TESTING
+                    return futureWeatherCode
+                else:
+                    last_dt = key_dt
+            else:
+                last_dt = key_dt
+
+            index += 1
+
+        # Could not get weather
+        # Use 'typical' Irish weather, i.e. '801, few clouds'
+        futureWeatherCode = 801
+        return futureWeatherCode
 
     #if app just started up...
     if hourSinceLastCall == 0:
         hourSinceLastCall = datetime.datetime.now() + datetime.timedelta(minutes=60)
         weatherCode = fetchRealTimeWeatherCode()
+        weatherCodeReturned = weatherCode
+        fetchRealTimeWeatherCode(hourSinceLastCall.timestamp())  # Update the forecast cache
 
     # else query OpenWeather API every hour
     currentCallTime = datetime.datetime.now()
-    if currentCallTime > hourSinceLastCall:
+    if currentCallTime > hourSinceLastCall: #If it's been an hour since last API call...
         hourSinceLastCall = currentCallTime + datetime.timedelta(minutes=60)
         #cache the weather code
         weatherCode = fetchRealTimeWeatherCode()
+        weatherCodeReturned = weatherCode
+    else: #use the cached current weather code
+        weatherCodeReturned = weatherCode
 
-    return weatherCode
+
+    # else query OpenWeather 5 day forecast API if timestamp provided
+    if currentCallTime > hourSinceLastCall and timeStamp is not None:
+        hourSinceLastCall = currentCallTime + datetime.timedelta(minutes=60)
+        futureWeatherCode = fetchRealTimeWeatherCode(timeStamp)
+        weatherCodeReturned = futureWeatherCode
+    elif timeStamp is not None: # else get cached data
+        futureWeatherCode = getCachedFutureWeather(timeStamp)
+        weatherCodeReturned = futureWeatherCode
+
+    return weatherCodeReturned
 
 """
 # RTPI request based on route and stop_id
--- busyapp/testing/get_weather.py
+++ busyapp/testing/get_weather.py
@@ -5,13 +5,15 @@ import os
 #get weather information
 hourSinceLastCall = 0 # type: float
 weatherCode = 0 # type: int
-futureWeatherCodes = []
+futureWeatherCodes = {}
 def getWeather(timeStamp: int = None):
     global hourSinceLastCall
     global weatherCode
+    global futureWeatherCodes
 
     #Dublin id for open weather: 7778677
     def fetchRealTimeWeatherCode(timeStamp: int = None):
+        global futureWeatherCodes
 
         if timeStamp is None: #Default to providing current weather
             r = requests.get('http://api.openweathermap.org/data/2.5/weather',
@@ -27,59 +29,73 @@ def getWeather(timeStamp: int = None):
                 return 801
         else: #Make a forecast based on provided timestamp
             r = requests.get('http://api.openweathermap.org/data/2.5/forecast',
-                             #params={'id': '7778677', 'APPID': os.environ.get('APPID')})
-                             params={'id': '7778677', 'APPID': ''})  # DO NOT COMMIT, DO NOT PUSH !!!!!!
-
-            # DO NOT COMMIT, DO NOT PUSH !!!!!!
-            # DO NOT COMMIT, DO NOT PUSH !!!!!!
-            # DO NOT COMMIT, DO NOT PUSH !!!!!!
+                             params={'id': '7778677', 'APPID': os.environ.get('APPID')})
 
             if r.status_code == requests.codes.ok:
                 weatherData = r.json()
 
-                #weatherCode = weatherData['weather'][0]['id']
-
-                #Check the cache....
-
-                futureWeatherCodes = [] #Empty the list
+                futureWeatherCodes = {} #Empty the dictionary
                 for row in weatherData['list']:
-                    print(row['dt'],':',row['weather'][0]['id'])
+                    #print(row['dt'],':',row['weather'][0]['id']) #FOR TESTING
+                    #Update the cache
+                    futureWeatherCodes[row['dt']] = row['weather'][0]['id']
 
-                    #Pupulate the cache
-                    futureWeatherCodes.append({'dt': row['dt'], 'id': row['weather'][0]['id']})
-
-                weatherCode = 1010
+                weatherCode = getCachedFutureWeather(timeStamp)
                 return weatherCode
             else:
                 # Could not get weather
                 # Use 'typical' Irish weather, i.e. '801, few clouds'
                 return 801
 
+    def getCachedFutureWeather(timeStamp: int = None):
+        index = 0
+        for key_dt in futureWeatherCodes:
+            print(key_dt, futureWeatherCodes[key_dt])
+            if index > 0:
+                next_dt = key_dt
+                if timeStamp < next_dt:
+                    futureWeatherCode = futureWeatherCodes[last_dt] #TESTING
+                    return futureWeatherCode
+                else:
+                    last_dt = key_dt
+            else:
+                last_dt = key_dt
+
+            index += 1
+
+        # Could not get weather
+        # Use 'typical' Irish weather, i.e. '801, few clouds'
+        futureWeatherCode = 801
+        return futureWeatherCode
 
     #if app just started up...
     if hourSinceLastCall == 0:
         hourSinceLastCall = datetime.datetime.now() + datetime.timedelta(minutes=60)
         weatherCode = fetchRealTimeWeatherCode()
         weatherCodeReturned = weatherCode
+        fetchRealTimeWeatherCode(hourSinceLastCall.timestamp())  # Update the forecast cache
 
     # else query OpenWeather API every hour
     currentCallTime = datetime.datetime.now()
-    if currentCallTime > hourSinceLastCall:
+    if currentCallTime > hourSinceLastCall: #If it's been an hour since last API call...
         hourSinceLastCall = currentCallTime + datetime.timedelta(minutes=60)
         #cache the weather code
         weatherCode = fetchRealTimeWeatherCode()
         weatherCodeReturned = weatherCode
+    else: #use the cached current weather code
+        weatherCodeReturned = weatherCode
 
 
-    # else query OpenWeather 5 day forecast API every 3 hours if timestamp provided
-
-    #if currentCallTime > hourSinceLastCall and timeStamp is not None:
-    if timeStamp is not None:
+    # else query OpenWeather 5 day forecast API if timestamp provided
+    if currentCallTime > hourSinceLastCall and timeStamp is not None:
         hourSinceLastCall = currentCallTime + datetime.timedelta(minutes=60)
-        #cache the weather code
         futureWeatherCode = fetchRealTimeWeatherCode(timeStamp)
+        weatherCodeReturned = futureWeatherCode
+    elif timeStamp is not None: # else get cached data
+        futureWeatherCode = getCachedFutureWeather(timeStamp)
         weatherCodeReturned = futureWeatherCode
 
     return weatherCodeReturned
 
-print(getWeather(1533114780)) # Tuesday, 1 August 2018 09:13:00 GMT
+#print("\nFirst request: ", getWeather(1533114780)) # Tuesday, 1 August 2018 09:13:00 GMT
+#print("\nSeconds request: ", getWeather(1533114780)) # Tuesday, 1 August 2018 09:13:00 GMT - test the cache
