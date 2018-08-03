ff033fdf2643bcd6119d8b83bad0a55c1437ae3c
JuliaBoes <juliaboes@posteo.de>
2018-08-03 12:03:31 +0100

    add time and date format info

--- busyapp/forms.py
+++ busyapp/forms.py
@@ -12,12 +12,12 @@ class PlannerForm(forms.Form):
     busnum_var = forms.CharField()
     from_var = forms.CharField()
     to_var = forms.CharField()
-    date_var = forms.DateField(widget=SelectDateWidget())  # Default formats '%Y-%m-%d','%m/%d/%Y', '%m/%d/%y'
-    time_var = forms.TimeField()
+    date_var = forms.DateField(widget=SelectDateWidget())    # Default formats '%d/%m/%Y', '%d/%m/%y'
+    time_var = forms.TimeField()    # Default format '%H:%M'
 
 
 class TouristForm(forms.Form):
     from_var_ex = forms.CharField()
     to_var_ex = forms.CharField()
-    date_var_ex = forms.DateField(widget=SelectDateWidget())  # Default formats '%Y-%m-%d','%m/%d/%Y', '%m/%d/%y'
-    time_var_ex = forms.TimeField()
+    date_var_ex = forms.DateField(widget=SelectDateWidget())    # Default formats '%d/%m/%Y', '%d/%m/%y'
+    time_var_ex = forms.TimeField()    # Default format '%H:%M'
